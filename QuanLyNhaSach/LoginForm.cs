9c36b8814ad027314b488bd30b6abcc39e156304
Ke Quang <trankequang9221@gmail.com>
2021-10-20 16:00:30 +0700

    m

--- QuanLyNhaSach/LoginForm.cs
+++ QuanLyNhaSach/LoginForm.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace QuanLyNhaSach
 {
-    class LoginForm
+    class LoginForm : Form
     {
+
     }
 }
