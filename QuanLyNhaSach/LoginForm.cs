6f86dc1fedf06d5f6bc1aba16b2142090d476702
Ke Quang <trankequang9221@gmail.com>
2021-10-20 16:36:42 +0700

    a

--- QuanLyNhaSach/LoginForm.cs
+++ QuanLyNhaSach/LoginForm.cs
@@ -9,6 +9,29 @@ namespace QuanLyNhaSach
 {
     class LoginForm : Form
     {
+        private Button button1;
 
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.button1);
+            this.Name = "LoginForm";
+            this.ResumeLayout(false);
+
+        }
     }
 }
